f9768c72088d6f37a9fad93c8b26ab6cd4f9c268
Rabehoavy <rabehoavy@users.noreply.github.com>
2019-03-09 23:51:08 +0100

    Update users with Eloquent

--- app/Http/Controllers/CompteController.php
+++ app/Http/Controllers/CompteController.php
@@ -23,6 +23,31 @@ class CompteController extends Controller
 
         flash("Vous êtes maintenant déconnecté.")->success();
 
-        return redirect('/');
+        return redirect('/connexion');
     }
+
+    public function modificationMotDePasse()
+    {
+        if (auth()->guest()) {
+            flash("Vous devez être connecté pour voir cette page.")->error();
+
+            return redirect('/connexion');
+        }
+
+        request()->validate([
+            'password' => ['required', 'confirmed', 'min:8'],
+            'password_confirmation' => ['required'],
+        ]);
+
+        // modifier l'utilisateur
+        auth()->user()->update([
+            'mot_de_passe' => bcrypt(request('password')),
+        ]);
+
+        flash("Votre mot de passe a bien été mis à jour.")->success();
+
+        return redirect('/mon-compte');
+
+    }
+
 }
